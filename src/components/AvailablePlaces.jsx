fa55ef2c4afaf28fa30b5ce7ac318a875ac698e7
luidimso <luidi_mso@outlook.com>
2023-11-19 16:17:31 -0300

    Sending a GET request via useEffect

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [avaliblesPlaces, setAvailablesPlaces] = useState([]);
+  const [availiblesPlaces, setAvailablesPlaces] = useState([]);
+
+  //this effect will be only runned when component built the first time, because there are no dependencies passed on the second paramter, in any case this effect will run again
+  useEffect(() => {
+    fetch("http://localhost:3000/places").then((response) => {
+      return response.json();
+    }).then((data) => {
+      setAvailablesPlaces(data.places);
+    });
+  }, []);
 
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availiblesPlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
