0123afc5460e96398c1f874ee5b8f5997b048a69
luidimso <luidi_mso@outlook.com>
2023-11-19 16:26:56 -0300

    Sending a GET request using async / await annotations on inside useEffect

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -6,11 +6,19 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   //this effect will be only runned when component built the first time, because there are no dependencies passed on the second paramter, in any case this effect will run again
   useEffect(() => {
-    fetch("http://localhost:3000/places").then((response) => {
-      return response.json();
-    }).then((data) => {
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const data = await response.json();
       setAvailablesPlaces(data.places);
-    });
+    }
+
+    fetchPlaces();
+
+    // fetch("http://localhost:3000/places").then((response) => {
+    //   return response.json();
+    // }).then((data) => {
+    //   setAvailablesPlaces(data.places);
+    // });
   }, []);
 
   return (
