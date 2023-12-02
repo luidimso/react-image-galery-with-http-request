7631d42fde634371d200db295b4e70bdea59e57f
Luidi Matheus <34170403+luidimso@users.noreply.github.com>
2023-12-02 05:50:34 -0300

    Making annotations about Tanstrack Query

--- src/https.js
+++ src/https.js
@@ -1,3 +1,5 @@
+//A library that does HTTPs requests as well, but with more possibilities is Tanstack Query
+//npm i @tanstack/react-query
 export async function getPlaces() {
     const response = await fetch("http://localhost:3000/places");
     const data = await response.json();
